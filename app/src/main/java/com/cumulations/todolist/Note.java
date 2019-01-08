6df707fca5dae2eb1967e4e64278ce44c6662a19
KedarnathSahu <kedarnath.sahu97@gmail.com>
2019-01-08 17:32:06 +0530

    Initial commit + added dependency + created @Entity

---
+++ app/src/main/java/com/cumulations/todolist/Note.java
@@ -0,0 +1,43 @@
+package com.cumulations.todolist;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private String priority;
+
+    public Note(String title, String description, String priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+}
