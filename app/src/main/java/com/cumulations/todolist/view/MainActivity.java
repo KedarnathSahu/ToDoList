9903d7adc01790f3e48896fcef74c656af40f654
KedarnathSahu <kedarnath.sahu97@gmail.com>
2019-01-08 19:50:18 +0530

    implemented ViewModel

--- app/src/main/java/com/cumulations/todolist/view/MainActivity.java
+++ app/src/main/java/com/cumulations/todolist/view/MainActivity.java
@@ -1,15 +1,34 @@
 package com.cumulations.todolist.view;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.cumulations.todolist.R;
+import com.cumulations.todolist.model.model.Note;
+import com.cumulations.todolist.viewmodel.NoteViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //TODO update recycler view
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
